abe910510e5bdae9915ae2fd30fd782dadf0402f
eideard-hm <edierhernandezmo@gmail.com>
2025-09-07 18:35:59 -0500

    Update metadata to use dynamic brand name and change Open Graph image format

--- src/metadata/main.ts
+++ src/metadata/main.ts
@@ -5,29 +5,29 @@ import { env } from '@/config/env';
 const prodUrl = env.siteUrl;
 
 export const mainMetadata: Metadata = {
-  title: 'Healing Hands.R | Massage Therapist',
+  title: `${env.brand} | Massage Therapist`,
   description: 'Home massage therapy 🏠 | Personalized treatments to prevent injuries and chronic pain, relieve muscle stiffness, and improve your overall health and wellness.',
   keywords: ['help', 'support', 'community'],
   authors: [{ name: 'Edier Hernandez', url: 'https://edier-hm.netlify.app/' }],
   creator: 'Edier Hernandez',
-  publisher: 'Healing Hands.R',
+  publisher: `${env.brand}`,
   openGraph: {
-    title: 'Healing Hands.R | Massage Therapist',
+    title: `${env.brand} | Massage Therapist`,
     description: 'Home massage therapy 🏠 | Personalized treatments to prevent injuries and chronic pain, relieve muscle stiffness, and improve your overall health and wellness.',
     url: prodUrl,
-    siteName: 'Healing Hands.R | Massage Therapist',
+    siteName: `${env.brand} | Massage Therapist`,
     images: [
       {
-        url: `${prodUrl}/og-image.jpg`,
+        url: `${prodUrl}/og-image.png`,
         width: 1200,
         height: 630,
-        alt: 'Healing Hands.R - Home massage therapy 🏠 | Personalized treatments to prevent injuries and chronic pain, relieve muscle stiffness, and improve your overall health and wellness.',
+        alt: `${env.brand} - Home massage therapy 🏠 | Personalized treatments to prevent injuries and chronic pain, relieve muscle stiffness, and improve your overall health and wellness.`,
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Healing Hands.R | Massage Therapist',
+    title: `${env.brand} | Massage Therapist`,
     description: 'Home massage therapy 🏠 | Personalized treatments to prevent injuries and chronic pain, relieve muscle stiffness, and improve your overall health and wellness.',
     images: [`${prodUrl}/twitter-image.jpg`],
   },
