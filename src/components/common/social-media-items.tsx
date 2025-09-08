f62f148479c3057b561a11f0cd4034eeb6ef0b74
eideard-hm <edierhernandezmo@gmail.com>
2025-09-07 20:12:05 -0500

    Refactor WhatsApp link handling in SocialMediaItems component

--- src/components/common/social-media-items.tsx
+++ src/components/common/social-media-items.tsx
@@ -6,31 +6,17 @@ import {
 } from '@tabler/icons-react';
 
 import { env } from '@/config/env';
-import { waLink } from '@/lib/whatsapp';
+import { waLinkWithEnv } from '@/lib/whatsapp';
 
 type SocialMediaItemsProps = {
   classList?: string;
 };
 
 export function SocialMediaItems({ classList }: SocialMediaItemsProps) {
-     const waHref = waLink(env.whatsAppNumber, env.waMessage);
+     const waHref = waLinkWithEnv()
 
   return (
     <ul className={`${classList}`}>
-      <li className='rounded-full p-2 shadow-lg hover:shadow-xl transition-shadow hover:scale-100'>
-        <a
-          href='mailto:services@healinghandsr.com'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-[#D44638] hover:text-red-500 transition duration-200'
-          aria-label='Email - Healing Hands.R'
-        >
-          <IconMail
-            size={24}
-            className='inline-block'
-          />
-        </a>
-      </li>
       <li className='rounded-full p-2 shadow-lg hover:shadow-xl transition-shadow hover:scale-100'>
         <a
           href={waHref}
@@ -45,6 +31,22 @@ export function SocialMediaItems({ classList }: SocialMediaItemsProps) {
           />
         </a>
       </li>
+
+      <li className='rounded-full p-2 shadow-lg hover:shadow-xl transition-shadow hover:scale-100'>
+        <a
+          href='mailto:services@healinghandsr.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-[#D44638] hover:text-red-500 transition duration-200'
+          aria-label='Email - Healing Hands.R'
+        >
+          <IconMail
+            size={24}
+            className='inline-block'
+          />
+        </a>
+      </li>
+      
       <li className='rounded-full p-2 shadow-lg hover:shadow-xl transition-shadow hover:scale-100'>
         <a
           href='https://www.facebook.com/HealingHands.R1'
@@ -59,6 +61,7 @@ export function SocialMediaItems({ classList }: SocialMediaItemsProps) {
           />
         </a>
       </li>
+
       <li className='rounded-full p-2 shadow-lg hover:shadow-xl transition-shadow hover:scale-100'>
         <a
           href='https://www.instagram.com/healinghands.r1'
