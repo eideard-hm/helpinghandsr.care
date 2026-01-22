8240a120e0ec159bb65a53d6e0828949bc6a490e
eideard-hm <edierhernandezmo@gmail.com>
2026-01-21 20:18:32 -0500

    feat(prisma): add Prisma configuration and update schema
    - Introduced a new Prisma configuration file (prisma.config.ts) to manage schema and migrations.
    - Updated schema.prisma to use the new Prisma client provider and removed directUrl.
    - Refactored prisma.ts to use PrismaPg adapter with PostgreSQL connection pooling.
    - Enhanced tsconfig.json for better type checking and included additional TypeScript paths.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,24 @@
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-export const prisma = new PrismaClient();
+import { PrismaClient } from '@/generated/prisma/client';
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg(
+  new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+);
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export { prisma };
