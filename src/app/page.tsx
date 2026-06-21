d8d3db089323878583bfa0f43c9cd083f2ce6414
eideard-hm <edierhernandezmo@gmail.com>
2026-06-20 20:01:54 -0500

    chore: hide pricing and service areas sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { About } from '@/components/ui/about/about';
 import { Faq } from '@/components/ui/faq';
 import { Hero } from '@/components/ui/hero';
 import { HowItWorks } from '@/components/ui/how-it-works/how-it-works';
-import { PricingGuide } from '@/components/ui/pricing-guide';
-import { ServiceAreas } from '@/components/ui/service-areas';
 import { Services } from '@/components/ui/services/services';
 import { Testimonials } from '@/components/ui/testimonials/testimonials';
 import { TrustBar } from '@/components/ui/trust-bar';
@@ -16,10 +14,6 @@ export default function Home() {
       <TrustBar />
 
       <Services />
-
-      <PricingGuide />
-
-      <ServiceAreas />
 
       <HowItWorks />
 
